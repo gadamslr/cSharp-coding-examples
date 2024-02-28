228786bbf76bff28d7826350bcbd6465d202b7d0
gadams <gadams@longroad.ac.uk>
2024-02-28 13:09:47 +0000

    Add project files.

---
+++ cSharp coding/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_coding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            arithmetic_operations.arithmetic_operations_code();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cSharp coding/arithmetic operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_coding
+{
+    internal class arithmetic_operations
+    {
+
+        static void arithmetic_operations_examples()
+        {
+
+        }
+    }
+}
